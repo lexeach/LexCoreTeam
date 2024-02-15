a8eaf0fc522e4305b765d2228c7cf92679458bf8
lexeach <80155340+lexeach@users.noreply.github.com>
2024-02-15 19:27:31 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -360,7 +360,8 @@ const Dashboard = () => {
       let ICU_ = new web3.eth.Contract(ICU.ABI, ICU.address);
       console.log("accoutn", account);
       let value_ = await ICU_.methods.REGESTRATION_FESS().call();
-      value_ = (Number(value_) * 10 + ((Number(value_) * 10) *30 / 100 )).toString();
+      let tax = await ICU.methods.taxRate().call();
+      value_ = (Number(value_) * 10 + ((Number(value_) * 10) * tax / 100 )).toString();
       value_ = await scientificToInteger(value_);
       let USDT_ = new web3.eth.Contract(USDT.ABI, USDT.address);
       await USDT_.methods
