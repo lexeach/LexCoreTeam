57da9e70d2fcd026bb24e4489894f80cb7ddbe87
lexeach <80155340+lexeach@users.noreply.github.com>
2024-02-23 14:29:53 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -364,7 +364,7 @@ let tax = await ICU_.methods.taxRate().call();
 
 // Apply tax rate to value_
 value_ = (Number(value_) + (Number(value_) * Number(tax) / 100)).toString();
-
+value_ = (Number(value_) * 10).toString();
 // Multiply the result by 10
 //let valueNew = Number(value_) * 10;
 
