66613e6704421b4199edeb6777bd2979e039e83c
lexeach <80155340+lexeach@users.noreply.github.com>
2024-02-23 14:23:15 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -359,25 +359,17 @@ const Dashboard = () => {
       const accounts = await web3.eth.requestAccounts();
       let ICU_ = new web3.eth.Contract(ICU.ABI, ICU.address);
       console.log("accoutn", account);
-    const BigNumber = require('bignumber.js');
-
-let value_ = await ICU_.methods.REGESTRATION_FESS().call();
+     let value_ = await ICU_.methods.REGESTRATION_FESS().call();
 let tax = await ICU_.methods.taxRate().call();
 
-// Convert value_ and tax to BigNumber
-value_ = new BigNumber(value_);
-tax = new BigNumber(tax);
-
 // Apply tax rate to value_
-value_ = value_.plus(value_.times(tax).dividedBy(100)).toString();
+value_ = (Number(value_) + (Number(value_) * Number(tax) / 100)).toString();
 
-// Multiply the result by 10 using BigNumber
-value_ = value_.times(10).toString();
+// Multiply the result by 10
+//let valueNew = Number(value_) * 10;
 
 // Convert to integer using scientificToInteger function
 value_ = await scientificToInteger(value_);
-
-   
       let USDT_ = new web3.eth.Contract(USDT.ABI, USDT.address);
       await USDT_.methods
         .approve(ICU.address, value_)
