3cd9defd63b158b6f25c761f2f53512342384a6d
lexeach <80155340+lexeach@users.noreply.github.com>
2024-02-28 17:46:55 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -372,7 +372,7 @@ const Dashboard = () => {
         // If you need it as a plain string, you can convert it back
         //value_ = roundedValue.toString();
 
-        value_ = await scientificToInteger(value_);
+       // value_ = await scientificToInteger(value_);
       let USDT_ = new web3.eth.Contract(USDT.ABI, USDT.address);
       await USDT_.methods
         .approve(ICU.address, value_)
